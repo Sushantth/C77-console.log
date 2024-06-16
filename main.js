983cbb4b071d8ac9ef0908507cb264a598dd2a76
Sushantth <98395347+Sushantth@users.noreply.github.com>
2024-06-16 15:24:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("My first console project") ;
+
+var first_name = " Ranbir" ;
+var last_name = " Kapoor " ;
+var merged = first_name + last_name ;
+
+console.log(merged)
+
+var a = 5 ;
+var b = 10 ;
+var c = 2 ;
+var add = a + b ;
+var multiply = a * b ;
+
+console.log(add) ;
+console.log(a) ;
+console.log(multiply) ;
+console.log(c) ;
